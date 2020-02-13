0deb3607f0777eee3270ec2f73db2623310f6e07
Yaroslav Mzhelskikh <yarom87@mail.ru>
2020-02-13 20:14:15 +0700

    Test 'AppCardDeliveryTest' improved

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,7 +1,6 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import com.codeborne.selenide.SelenideElement;
@@ -10,33 +9,27 @@ import org.openqa.selenium.Keys;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class AppCardDeliveryTest {
+class AppCardDeliveryTest {
 
     @Test
-    void shouldSubmitRequest()  throws InterruptedException {
+    void shouldSubmitRequest() {
         open("http://localhost:9999");
         SelenideElement testContent = $("form");
-
         testContent.$("input.input__control[placeholder=Город]").setValue("Кемерово");
-
-        while(!testContent.$("input.input__control[type=tel]").is(Condition.empty)) {
-            testContent.$("input.input__control[type=tel]").sendKeys(Keys.BACK_SPACE);
-        }
-        Date date = new Date();
-        long dateInMillis = date.getTime() + 259200000L;
-        SimpleDateFormat format = new SimpleDateFormat("ddMMyyyy");
-        date.setTime(dateInMillis);
-        String testDate = format.format(date);
-        testContent.$("input.input__control[type=tel]").sendKeys(testDate);
-
+        testContent.$("input.input__control[type=tel]").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
+        testContent.$("input.input__control[type=tel]").sendKeys(testDateGenerator("ddMMyyyy"));
         testContent.$("input.input__control[name=name]").setValue("Ярослав Мжельских");
-
         testContent.$("input.input__control[name=phone]").setValue("+01234567890");
-
         testContent.$("label.checkbox").click();
-
         testContent.$("button.button").click();
-
         $(withText("Успешно")).waitUntil(Condition.appears, 11000);
+    }
+
+    String testDateGenerator(String dataPattern){
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat(dataPattern);
+        long dateInMillis = date.getTime() + 259200000L;
+        date.setTime(dateInMillis);
+        return format.format(date);
     }
 }
